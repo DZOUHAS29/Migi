6241689909315da2f2d9dd0126e9716368367983
Johana Pribanová <johanapribanova@Johanas-MacBook-Air.local>
2024-04-10 16:01:52 +0200

    data for graphs are complete

--- app/profile/components/ProfileGraphs.tsx
+++ app/profile/components/ProfileGraphs.tsx
@@ -5,11 +5,58 @@ import { retrieveStats } from '@/app/middleware/retrieveStats'
 import { getRecords } from '@/app/record-actions'
 import moment from 'moment'
 import React, { useEffect, useState } from 'react'
-import { Bar, Doughnut, Line } from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
+interface GraphProps {
+    parts: {
+        mornings: number;
+        afternoons: number;
+        evenings: number;
+    },
+    types: {
+        headaches: number;
+        migraines: number;
+    }
+}
+
+const defaultStat = {
+    parts: {
+        mornings: 0,
+        afternoons: 0,
+        evenings: 0,
+    },
+    types: {
+        headaches: 0,
+        migraines: 0,
+    }
+}
 
 export const ProfileGraphs = () => {
-    const [current, setCurrent] = useState<StatsProps>();
-    const [previous, setPrevious] = useState<StatsProps>();
+    const [current, setCurrent] = useState<GraphProps>(defaultStat);
+    const [previous, setPrevious] = useState<GraphProps>(defaultStat);
 
     useEffect(() => {
         init();
@@ -26,7 +73,12 @@ export const ProfileGraphs = () => {
 
         const stats = retrieveStats(data, days);
 
-        console.log("ahoj");
+        if (!stats)
+            return;
+
+        const formattedStats = formatData(stats);
+
+        setPrevious(formattedStats);
     }
 
     const getcurrentMonth = async (): Promise<void> => {
@@ -40,7 +92,12 @@ export const ProfileGraphs = () => {
 
         const stats = retrieveStats(data, days);
 
-        console.log("ahoj");
+        if (!stats)
+            return;
+
+        const formattedStats = formatData(stats);
+
+        setCurrent(formattedStats);
     }
 
     const getData = async (days: string[]): Promise<RecordsProps[] | string> => {
@@ -52,8 +109,19 @@ export const ProfileGraphs = () => {
         return data;
     }
 
-    const formatData = (stats: StatsProps[]) => {
-        const mornings = stats.filter(({ part: { morning } }) => { })
+    const formatData = (stats: StatsProps[]): GraphProps => {
+        const parts = {
+            mornings: stats.filter(({ part: { morning } }) => morning > 0).length,
+            afternoons: stats.filter(({ part: { afternoon } }) => afternoon > 0).length,
+            evenings: stats.filter(({ part: { evening } }) => evening > 0).length,
+        }
+
+        const types = {
+            headaches: stats.filter(({ headaches }) => headaches > 0).length,
+            migraines: stats.filter(({ migraines }) => migraines > 0).length
+        }
+
+        return { parts, types };
     }
 
     const init = (): void => {
@@ -65,7 +133,29 @@ export const ProfileGraphs = () => {
         <div className='flex flex-col'>
             <div className='flex'>
                 <div>
-                    {/* Dayparts <Doughnut /> */}
+                    {
+                        (() => {
+                            if (current && current.parts) {
+                                return (
+                                    <Doughnut
+                                        data={{
+                                            labels: Object.keys(current.parts).map(part => part),
+                                            datasets: [{
+                                                label: 'ahoj',
+                                                data: Object.keys(current.parts).map(part => current.parts[part as keyof typeof current.parts]),
+                                                backgroundColor: [
+                                                    'rgb(255, 99, 132)',
+                                                    'rgb(54, 162, 235)',
+                                                    'rgb(255, 205, 86)'
+                                                ],
+                                                hoverOffset: 4
+                                            }]
+                                        }}
+                                    />
+                                );
+                            }
+                        })()
+                    }
                 </div>
                 <div>
                     {/*  Type <Bar /> */}
